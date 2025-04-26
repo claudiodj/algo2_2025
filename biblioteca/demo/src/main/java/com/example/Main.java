dba3a29c09ebbf59e146dc2e5bc66392c0ca4849
Claudio <claudio_dj@hotmail.com>
2025-04-26 00:01:22 -0300

    Archivos actualizados con lo visto hasta el 25/04/2025

--- biblioteca/demo/src/main/java/com/example/Main.java
+++ biblioteca/demo/src/main/java/com/example/Main.java
@@ -49,91 +49,43 @@ public class Main {
 
         } while (opcion.equalsIgnoreCase("s"));
 
+        System.out.println("Cantidad de libros de la lista : " + listaLibros.size());
         // Mostrar todos los libros ingresados
         System.out.println("\n--- Lista de libros ingresados ---");
         for (libros lib : listaLibros) {
             System.out.println(lib);
         }
 
-        /*
-         * 
-         * // Ingreso de personas
-         * do {
-         * persona p = new persona();
-         * 
-         * System.out.print("Ingrese nombre de la persona: ");
-         * p.setNombre(scanner.nextLine());
-         * 
-         * System.out.print("Ingrese apellido: ");
-         * p.setApellido(scanner.nextLine());
-         * 
-         * System.out.print("Ingrese DNI: ");
-         * p.setDni(scanner.nextInt());
-         * 
-         * scanner.nextLine(); // limpiar buffer
-         * 
-         * System.out.print("Ingrese email: ");
-         * p.setEmail(scanner.nextLine());
-         * 
-         * listaPersonas.add(p);
-         * 
-         * System.out.print("¿Desea ingresar otra persona? (s/n): ");
-         * opcion = scanner.nextLine();
-         * 
-         * } while (opcion.equalsIgnoreCase("s"));
-         * 
-         * System.out.println("\n--- Lista de personas ingresadas ---");
-         * for (persona p : listaPersonas) {
-         * System.out.println(p);
-         * }
-         * 
-         */
+        // Ingreso de personas
+        do {
+            persona p = new persona();
 
-        // System.out.println("Hello world!");
-        // aca voy a declarar la variable libro1 vacia
-        // libros libro1 = new libros();
-        // libros libro3 = new libros();
-        /*
-         * aca declaro la variable libro2
-         * con los datos
-         * de cada
-         * parametro
-         */
-        // libros libro2 = new libros("El Principito","Saint
-        // Exupery","Galerna",123,"Fantasia",false);
+            System.out.print("Ingrese nombre de la persona: ");
+            p.setNombre(teclado.nextLine());
 
-        // System.out.println("libro 1: " + libro1.toString());
-        // System.out.println("libro 2: " + libro2.toString());
+            System.out.print("Ingrese apellido: ");
+            p.setApellido(teclado.nextLine());
 
-        // persona persona1 = new persona();
+            System.out.print("Ingrese DNI: ");
+            p.setDni(teclado.nextInt());
 
-        // persona1.setNombre("Pepe");
-        // persona1.setApellido("Biondi");
+            teclado.nextLine(); // limpiar buffer
 
-        // persona1.setDni(1234);
-        // persona1.setEmail("pepe_biondi@pepe.com");
+            System.out.print("Ingrese email: ");
+            p.setEmail(teclado.nextLine());
 
-        // System.out.println(persona1.toString());
+            listaPersonas.add(p);
 
-        // libro1.setNombre("Cien años de soledad");
-        // libro1.nombre = "Cien años de soledad";
+            System.out.print("¿Desea ingresar otra persona? (s/n): ");
+            opcion = teclado.nextLine();
 
-        // persona persona5 = new persona();
+        } while (opcion.equalsIgnoreCase("s"));
 
-        // persona5.setNombre("Juan");
-
-        // System.out.println(persona5.toString());
-
-        // libro1.nombre = "El capitan Garfio";
-        // libro1.autor = "Celine Dione";
-
-        // libro1.setAutor("Gabriel García Márquez");
-
-        // System.out.println("El autor del libro1 es : " + libro1.getAutor());
-
-        // System.out.println("libro 1: " + libro1.toString());
-
-        // System.out.println("El nombre del libro1 es : " + libro1.getNombre());
+        System.out.println("Cantidad de personas agregadas a la lista: " + listaPersonas.size());
+        System.out.println("\n--- Lista de personas ingresadas ---");
+        for (persona p : listaPersonas) {
+            System.out.println(p);
+        }
 
         teclado.close();
 
